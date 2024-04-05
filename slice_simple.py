d52f625c5c32b1376f8ac211609f2d3dd16fc252
mayraaoteo <162721507+mayraaoteo@users.noreply.github.com>
2024-04-05 11:05:32 -0300

    Entrego slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,5 @@
 def slice_simple():
-    texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
-
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+    txt = "Awesome"
+    print(txt.lower()[0: 3])
+    print(txt.lower()[2: 5])
+    print(txt.lower()[0: 4] + txt.lower()[-3: ])
